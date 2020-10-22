537400700ed1d4bc29fb7579c7a5e150a372b58f
Jever Osmanova <amidala274@gmail.com>
2020-10-22 23:33:30 +0300

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { AppGuard } from './app.guard';
 
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./app.module').then(m => m.AppModule),
-    canActivate: [AppGuard]
-  },
-  {
     path: 'authorization',
     loadChildren: () => import('./authorization/authorization.module').then(m => m.AuthorizationModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule),
+    canActivate: [AppGuard]
   }
 ];
 
