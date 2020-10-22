3c6e7d55107f948066582ea36144099ed6c1d22a
Jever Osmanova <amidala274@gmail.com>
2020-10-22 23:32:25 +0300

    Contacts component added

---
+++ src/app/contacts/contacts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactsComponent } from './contacts.component';
+import { ContactsRoutingModule } from './contacts-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ContactsRoutingModule
+  ],
+  declarations: [ContactsComponent]
+})
+export class ContactsModule { }
+
