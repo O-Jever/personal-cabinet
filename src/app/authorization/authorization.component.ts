9152b8a2d821f55e3d3e3754da5b1713d093fb5d
Jever Osmanova <amidala274@gmail.com>
2020-10-22 23:31:23 +0300

    Authorization component added

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../interfaces/user.interface';
+import { AuthorizationService } from '../services/authorization.service';
 
 @Component({
   selector: 'app-authorization',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorizationComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+  private sessionStorage: Storage;
+  public return;
+
+  constructor(private router: Router, private authorizationService: AuthorizationService, private route: ActivatedRoute) { 
+    this.sessionStorage = window.sessionStorage;
+  }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      login: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+    this.route.queryParams.subscribe(params => this.return = params['return']);
+  }
+
+
+  public onSubmit() {
+    console.log(this.loginForm.value);
+    this.authorizationService.authorization(this.loginForm.value).subscribe((user: User) => {
+      this.sessionStorage.setItem('hash', user.hash);
+      this.router.navigateByUrl(this.return);
+    });
   }
 
 }
--- src/app/authorization/authorization.module.ts
+++ src/app/authorization/authorization.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorizationComponent } from './authorization.component';
 import { AuthorizationRoutingModule } from './authorization-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthorizationRoutingModule
+    AuthorizationRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
   declarations: [AuthorizationComponent]
 })
